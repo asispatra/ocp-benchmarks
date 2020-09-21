187f9cb2bca28a2cd2d3020b8c838b5c14fb6fed
Asis Patra <asis.patra0425@gmail.com>
2020-09-21 13:51:24 +0530

    Update start_benchmark_and_collect_data.sh

--- scripts/start_benchmark_and_collect_data.sh
+++ scripts/start_benchmark_and_collect_data.sh
@@ -2,6 +2,8 @@
 
 bash getmemory_data.sh > MEMORY.log 2>&1 &
 echo $! > PID
+bash getfree_data.sh > FREE.log 2>&1 &
+echo $! >> PID
 
 echo "### Starting MMTests..."
 time echo yes | numactl -C 0-3 mmtests_CI/run-mmtests.sh --config mmtests_CI/configs/config-workload-wp-tlbflush C_TLBFLUSH_1
@@ -9,9 +11,26 @@ echo "### MMTests DONE!"
 
 kill $(cat PID) ; rm PID
 #cat MEMORY.log
+echo "* * * * * MEMORY.log  * * * *"
 echo "### ONLY ACTUAL RUN"
-cat MEMORY.log | grep '+++' | cut -d ' ' -f2 | jq -s 'min,max'
+cat MEMORY.log | grep '+++' | cut -d ' ' -f2- | jq -s 'min,max'
 echo "### ONLY OUTSIDE ACTUAL RUN"
-cat MEMORY.log | grep '===' | cut -d ' ' -f2 | jq -s 'min,max'
+cat MEMORY.log | grep '===' | cut -d ' ' -f2- | jq -s 'min,max'
 echo "### THROUGHOUT RUN"
-cat MEMORY.log | grep -e '+++' -e '===' | cut -d ' ' -f2 | jq -s 'min,max'
+cat MEMORY.log | grep -e '+++' -e '===' | cut -d ' ' -f2- | jq -s 'min,max'
+
+#cat FREE.log
+echo "* * * * * FREE.log  * * * *"
+echo "### USED: ONLY ACTUAL RUN"
+cat FREE.log | grep '+++' | cut -d ' ' -f2- | grep 'Mem:'| tr -s ' ' | cut -d ' ' -f3 | jq -s 'min,max'
+echo "### USED: ONLY OUTSIDE ACTUAL RUN"
+cat FREE.log | grep '===' | cut -d ' ' -f2- | grep 'Mem:'| tr -s ' ' | cut -d ' ' -f3 | jq -s 'min,max'
+echo "### USED: THROUGHOUT RUN"
+cat FREE.log | grep -e '+++' -e '===' | cut -d ' ' -f2- | grep 'Mem:'| tr -s ' ' | cut -d ' ' -f3 | jq -s 'min,max'
+
+echo "### CACHED: ONLY ACTUAL RUN"
+cat FREE.log | grep '+++' | cut -d ' ' -f2- | grep 'Mem:'| tr -s ' ' | cut -d ' ' -f7 | jq -s 'min,max'
+echo "### CACHED: ONLY OUTSIDE ACTUAL RUN"
+cat FREE.log | grep '===' | cut -d ' ' -f2- | grep 'Mem:'| tr -s ' ' | cut -d ' ' -f7 | jq -s 'min,max'
+echo "### CACHED: THROUGHOUT RUN"
+cat FREE.log | grep -e '+++' -e '===' | cut -d ' ' -f2- | grep 'Mem:'| tr -s ' ' | cut -d ' ' -f7 | jq -s 'min,max'
