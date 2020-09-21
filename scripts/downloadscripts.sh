bd9fb5e10c4e3ca864e6e81c9be4b53853f3a17e
Asis Patra <asis.patra0425@gmail.com>
2020-09-21 12:59:01 +0530

    Update downloadscripts.sh

--- scripts/downloadscripts.sh
+++ scripts/downloadscripts.sh
@@ -2,4 +2,6 @@
 
 echo "Downloading scripts..."
 
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/scripts/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
+
 echo "Scripts download DONE!"
