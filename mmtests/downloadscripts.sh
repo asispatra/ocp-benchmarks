3e51a2c8d82115237101955f77da59fe505f262f
Asis Patra <asis.patra0425@gmail.com>
2020-10-08 15:55:21 +0530

    Update downloadscripts.sh

--- mmtests/downloadscripts.sh
+++ mmtests/downloadscripts.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+echo "Downloading scripts..."
+
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getmemory_data.sh" -O getmemory_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getfree_data.sh" -O getfree_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getkmem_data.sh" -O getkmem_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/mmtests/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
+
+echo "Scripts download DONE!"
 
