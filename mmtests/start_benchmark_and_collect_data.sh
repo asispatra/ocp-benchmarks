44b692c1d64595311857e0dec5328e4cea9d6c3c
Asis Patra <asis.patra0425@gmail.com>
2020-10-15 13:42:43 +0530

    Update start_benchmark_and_collect_data.sh

--- mmtests/start_benchmark_and_collect_data.sh
+++ mmtests/start_benchmark_and_collect_data.sh
@@ -3,6 +3,12 @@
 #sed -i 's/PROCESSES=4/PROCESSES=$NUMCPUS/g' mmtests_CI/configs/config-workload-wp-tlbflush 
 #sed -i 's/PROCESSES=4/PROCESSES=8/g' mmtests_CI/configs/config-workload-wp-tlbflush 
 
+if echo "$@" | grep workload-wp-tlbflush > /dev/null; then 
+  sed -i 's/5000$/50000/' mmtests_CI/shellpack_src/src/wptlbflush/wptlbflush-install
+elif echo "$@" | grep db-sqlite-insert > /dev/null; then 
+  sed -i 's/SQLITE_SIZE=.*$/SQLITE_SIZE=10000/' mmtests_CI/configs/config-db-sqlite-insert-small
+fi
+
 #bash getmemory_data.sh > MEMORY.log 2>&1 &
 #echo $! > PID
 bash getfree_data.sh > FREE.log 2>&1 &
