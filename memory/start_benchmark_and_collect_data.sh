69051752652cb8501c1de02bd76be4d615212502
Asis Patra <asis.patra0425@gmail.com>
2021-01-27 12:33:58 -0600

    Memory script updated

--- memory/start_benchmark_and_collect_data.sh
+++ memory/start_benchmark_and_collect_data.sh
@@ -2,7 +2,17 @@
 
 #sed -i 's/PROCESSES=4/PROCESSES=$NUMCPUS/g' mmtests_CI/configs/config-workload-wp-tlbflush 
 #sed -i 's/PROCESSES=4/PROCESSES=8/g' mmtests_CI/configs/config-workload-wp-tlbflush 
-sed -i 's/5000$/50000/' mmtests_CI/shellpack_src/src/wptlbflush/wptlbflush-install
+
+if echo "$@" | grep workload-wp-tlbflush > /dev/null; then 
+  sed -i 's/5000$/50000/' mmtests_CI/shellpack_src/src/wptlbflush/wptlbflush-install
+  #sed -i 's/PROCESSES=4/PROCESSES=16/g' mmtests_CI/configs/config-workload-wp-tlbflush 
+elif echo "$@" | grep db-sqlite-insert > /dev/null; then 
+  sed -i 's/SQLITE_SIZE=.*$/SQLITE_SIZE=50000/' mmtests_CI/configs/config-db-sqlite-insert-small
+elif echo "$@" | grep io-pgioperf > /dev/null; then
+  sed -i 's/PGIOPERF_SAMPLES=.*$/PGIOPERF_SAMPLES=30000/' mmtests_CI/configs/config-io-pgioperf
+elif echo "$@" | grep workload-thpscale > /dev/null; then
+  sed -i 's/THPSCALE_MAPSIZE=.*$/THPSCALE_MAPSIZE=21474836480/' mmtests_CI/configs/config-workload-thpscale
+fi
 
 #bash getmemory_data.sh > MEMORY.log 2>&1 &
 #echo $! > PID
@@ -12,7 +22,8 @@ bash getmem_data.sh > KMEM.log 2>&1 &
 echo $! >> PID
 
 echo "### Starting MMTests..."
-time echo yes | mmtests_CI/run-mmtests.sh --config mmtests_CI/configs/config-workload-wp-tlbflush C_TLBFLUSH_1
+echo "$@"
+eval "$@"
 echo "### MMTests DONE!"
 
 sleep 5
@@ -60,6 +71,16 @@ for PATTERN in "### MEM WORKER:" "### MEM kubepods:" "### MEM kubepods-.*pod" "#
   cat KMEM.log | grep -e '+++' -e '===' | grep "$PATTERN" | sed 's/.*: \(.*\)/\1/g' | jq -s 'max'
 done | paste -s -d ' '
 
+
 # Performance Data
 echo "### PERFORMANCE"
-cat  mmtests_CI/work/log/C_TLBFLUSH_1/iter-0/wptlbflush/logs/wp-tlbflush-*.log | jq -s 'min,max,add/length'
+if echo "$@" | grep workload-wp-tlbflush > /dev/null; then 
+  #cat  mmtests_CI/work/log/C_TLBFLUSH_1/iter-0/wptlbflush/logs/wp-tlbflush-*.log | jq -s 'min,max,add/length'
+  mmtests_CI/bin/compare-mmtests.pl --directory mmtests_CI/work/log --benchmark wptlbflush --names C_TLBFLUSH_1
+elif echo "$@" | grep db-sqlite-insert > /dev/null; then 
+  cat mmtests_CI/work/log/C_SQLITE_1/iter-0/sqlite/logs/sqlite*.time
+elif echo "$@" | grep io-pgioperf > /dev/null; then
+  mmtests_CI/bin/compare-mmtests.pl --directory mmtests_CI/work/log --benchmark pgioperfbench --names C_PGIOPERF_1
+elif echo "$@" | grep workload-thpscale > /dev/null; then
+  mmtests_CI/bin/compare-mmtests.pl --directory mmtests_CI/work/log --benchmark thpscale --names C_THPSCALE_1
+fi
--- scripts/downloadscripts.sh
+++ scripts/downloadscripts.sh
@@ -7,14 +7,9 @@ echo "Downloading scripts..."
 #wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/scripts/getkmem_data.sh" -O getkmem_data.sh
 #wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/scripts/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
 
-#wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getmemory_data.sh" -O getmemory_data.sh
-#wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getfree_data.sh" -O getfree_data.sh
-#wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getkmem_data.sh" -O getkmem_data.sh
-#wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
-
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getmemory_data.sh" -O getmemory_data.sh
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getfree_data.sh" -O getfree_data.sh
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getkmem_data.sh" -O getkmem_data.sh
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getmemory_data.sh" -O getmemory_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getfree_data.sh" -O getfree_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getkmem_data.sh" -O getkmem_data.sh
+wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
 
 echo "Scripts download DONE!"
