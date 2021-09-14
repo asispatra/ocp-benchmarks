f68560361511e7370c718115ceddff2ceb62ced3
Asis Patra <asis.patra0425@gmail.com>
2021-09-14 21:20:42 +0530

    Update downloadscripts.sh

--- mmtests/downloadscripts.sh
+++ mmtests/downloadscripts.sh
@@ -7,10 +7,10 @@ echo "Downloading scripts..."
 #wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/full/getkmem_data.sh" -O getkmem_data.sh
 #wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/mmtests/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
 
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getmemory_data.sh" -O getmemory_data.sh
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getfree_data.sh" -O getfree_data.sh
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getmem_data.sh" -O getmem_data.sh
-wget "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/start_benchmark_and_collect_data.sh" -O start_benchmark_and_collect_data.sh
+curl "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getmemory_data.sh" -o getmemory_data.sh
+curl "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getfree_data.sh" -o getfree_data.sh
+curl "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/getmem_data.sh" -o getmem_data.sh
+curl "https://raw.githubusercontent.com/asispatra/ocp-benchmarks/master/memory/start_benchmark_and_collect_data.sh" -o start_benchmark_and_collect_data.sh
 
 echo "Scripts download DONE!"
 
